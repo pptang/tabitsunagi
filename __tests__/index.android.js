ea3da763781d87736db55142e70b768ea402100a
pptang <ptang8@bus.illinois.edu>
2017-03-10 21:45:09 +0900

    1. add test snapshot 2. add travis config

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -5,8 +5,12 @@ import Index from '../index.android.js';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+describe('Android Index Test', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <Index />
+    );
+    expect(tree).toMatchSnapshot();
+  });
 });
+
