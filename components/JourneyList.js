11676af3192e740b219900f04c0a516f6faa9aba
pptang <ptang8@bus.illinois.edu>
2017-03-15 21:00:00 +0900

    1. add whole workflow and some styles

--- components/JourneyList.js
+++ components/JourneyList.js
@@ -2,39 +2,50 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  Alert,
   ListView,
-  TouchableHighlight
+  TouchableHighlight,
+  Image
 } from 'react-native';
 
-const JOURNEY_API = 'https://tabitsunagi.com/journey/';
+import { JOURNEY_SCHEDULE_API } from '../utils/constants';
+import Separator from './ListViewSeparator';
+
 export default class JourneyList extends Component {
   constructor(props) {
     super(props);
-    this.renderRow = this.renderRow.bind(this);
+    this.renderJourney = this.renderJourney.bind(this);
+    this.fetchJourneySchedule = this.fetchJourneySchedule.bind(this);
     this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
   }
 
   onPressJourney(journey) {
-    this.fetchJourney(journey.id);
+    this.fetchJourneySchedule(journey.id);
   }
 
-  fetchJourney(id) {
-    const url = `${JOURNEY_API}${id}`;
+  fetchJourneySchedule(journeyId) {
+    const url = `${JOURNEY_SCHEDULE_API}${journeyId}`;
     fetch(url)
       .then(res => res.json())
       .then((res) => {
-        Alert.alert(res.journey.journeySpots[0].title);
+        this.props.navigate('JourneySchedule', {
+          title: res.journey.title,
+          journey: res.journey
+        });
       })
       .catch(err => console.error(err));
   }
 
-  renderRow(rowData) {
+  renderJourney(journey) {
     return (
-      <TouchableHighlight onPress={() => this.onPressJourney(rowData)}>
-        <Text style={{ margin: 10 }}>
-          {rowData.title}
-        </Text>
+      <TouchableHighlight onPress={() => this.onPressJourney(journey)}
+        underlayColor='#009688'>
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+          <Image source={{ uri: journey.picture }}
+            style={{ width: 75, height: 75 }}/>
+          <Text style={{ margin: 10 }}>
+            {journey.title}
+          </Text>
+        </View>
       </TouchableHighlight>
     );
   }
@@ -46,7 +57,9 @@ export default class JourneyList extends Component {
       <View>
         <ListView
           dataSource={this.ds.cloneWithRows(journeys)}
-          renderRow={this.renderRow}
+          renderRow={this.renderJourney}
+          renderSeparator={Separator}
+          enableEmptySections={true}
         />
       </View>
     );
--- index.android.js
+++ index.android.js
@@ -1,160 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  ListView,
-  TouchableHighlight
+  Navigator
 } from 'react-native';
-import JourneyList from './components/JourneyList';
+import { StackNavigator } from 'react-navigation';
 
-const AUTO_COMPLETE_API = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?';
-const API_KEY = 'AIzaSyA9THtJDf8dBNdq6sLKHdq87B3vVbQpz28';
-const JOURNEY_LIST_API = 'https://tabitsunagi.com/journey/load?';
-export default class tabitsunagi extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-    this.fetchPredictions = this.fetchPredictions.bind(this);
-    this.onPressPrediction = this.onPressPrediction.bind(this);
-    this.renderRow = this.renderRow.bind(this);
-    this.onSearch = this.onSearch.bind(this);
-    this.state = {
-      text: '',
-      placeId: '',
-      predictions: this.ds.cloneWithRows([]),
-      isOpen: false,
-      journeys: []
-    };
-  }
+import SearchPage from './components/SearchPage';
+import JourneySchedule from './components/JourneySchedule';
+import SpotList from './components/SpotList';
 
-  onSearch() {
-    this.fetchJourneyList(this.state.placeId);
-  };
-
-  fetchPredictions(text) {
-    this.setState({ text });
-    const url = `${AUTO_COMPLETE_API}input=${text}&type=geocode&language=en&key=${API_KEY}`;
-    fetch(url)
-      .then(res => res.json())
-      .then((res) => {
-        this.setState({ 
-          predictions: this.ds.cloneWithRows(res.predictions),
-          isOpen: res.predictions.length > 0,
-          placeId: res.predictions.length > 0 ? res.predictions[0].place_id : ''
-        });
-      })
-      .catch(err => console.error(err));
-  }
-
-  fetchJourneyList(placeId) {
-    const url = `${JOURNEY_LIST_API}spots=${placeId}&start=30&fuzzy=true&months=1,2,3,4,5,6,7,8,9,10,11,12&day=any`;
-    fetch(url)
-      .then(res => res.json())
-      .then((res) => {
-        this.setState({
-          journeys: res.journeys
-        });
-      })
-      .catch(err => console.error(err));
-  }
-
-  onPressPrediction(prediction) {
-    this.setState({
-      text: prediction.description,
-      placeId: prediction.place_id,
-      isOpen: false
-    });
-  }
-
-  renderRow(rowData) {
-    return (
-      <TouchableHighlight onPress={() => this.onPressPrediction(rowData)}>
-        <Text style={{ margin: 10 }}>
-          {rowData.description}
-        </Text>
-      </TouchableHighlight>
-    );
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Tabitsunagi!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, enter your destination
-        </Text>
-        <View style={{
-          flexDirection: 'row',
-          justifyContent: 'center'
-        }}>
-          <View style={{ flex: 4 }}>
-            <TextInput
-              style={{
-                height: 40,
-              }}
-              value={this.state.text}
-              placeholder="Taipei 101, Tokyo...."
-              onChangeText={this.fetchPredictions}
-            />
-            {
-              this.state.isOpen ?
-                <View style={{
-                  borderLeftWidth: 1,
-                  borderRightWidth: 1,
-                  borderBottomWidth: 1,
-                  borderColor: '#841584'
-                }}>
-                  <ListView
-                    dataSource={this.state.predictions}
-                    renderRow={this.renderRow}
-                  />
-                </View> : null
-            }
-          </View>
-          <View style={{ height: 40, flex: 1 }}>
-            <Button
-              onPress={this.onSearch}
-              title="Search"
-              color="#841584"
-            />
-          </View>
-        </View>
-        <JourneyList journeys={this.state.journeys} />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const App = StackNavigator({
+  SearchPage: { screen: SearchPage },
+  JourneySchedule: { screen: JourneySchedule },
+  SpotList: { screen: SpotList }
 });
 
-AppRegistry.registerComponent('tabitsunagi', () => tabitsunagi);
+AppRegistry.registerComponent('tabitsunagi', () => App);
